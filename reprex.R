dd995a2ee883b7aec63485ef3ed39de9e9153268
richelbilderbeek <richel@richelbilderbeek.nl>
2020-04-21 13:26:47 +0200

    Run full reprex

--- reprex.R
+++ reprex.R
@@ -30,7 +30,7 @@ input_model <- create_inference_model(
 # Use the default BEAUti settings to create a BEAST2 input file
 create_beast2_input_file_from_model(
   input_filename,
-  output_filename,
+  output_filename = "irrelevant.xml",
   inference_model = input_model
 )
 
